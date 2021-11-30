4aeb2ab74e3d7403fc7a2aad18820e8c35da1fdd
Maxim Vinokurov <vmstr8@yandex.ru>
2021-11-30 14:25:25 +0300

    Мелкие изменения. Конкретно переименовал базу данных.

--- vkbot_sql/database.py
+++ vkbot_sql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./vk_bot.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./vk_bot.sqlite3"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
