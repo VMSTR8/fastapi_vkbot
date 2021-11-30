380bd3e03f4f3e76745007dfaa4953fba9ea7f77
Maxim Vinokurov <vmstr8@yandex.ru>
2021-11-30 21:16:33 +0300

    Сделано так, что все прилетающие данные собираются в список кортежей

--- vkbot_parser/parser_app.py
+++ vkbot_parser/parser_app.py
@@ -1,7 +1,37 @@
+import time
+
 from vkbot_parser.vk_parser import AlbumsParser
 
 app = AlbumsParser()
 
 if __name__ == '__main__':
-    photos = app.get_photos('esg')
-    print(app.get_comments('esg', all_photos=photos))
+    start_time = time.time()
+    photos = app.get_photos(group_name='esg')
+    photos_and_comments = app.get_comments(group_name='esg', all_photos=photos)
+
+    write_to_db = []
+
+    for value in photos_and_comments.values():
+        link = value['link']
+        if value['text'] != '':
+            try:
+                if value['comments'][-1] != '':
+                    text = f"Описание фото: {value['text']}.\n\n1-ый комментарий к фото: " \
+                           f"{value['comments'][-1]}".replace(u'\xa0', u'')
+                else:
+                    text = f"Описание фото: {value['text']}".replace(u'\xa0', u'')
+            except IndexError as err:
+                continue
+        else:
+            try:
+                if value['comments'][-1] != '':
+                    text = f"1-ый комментарий к фото: {value['comments'][-1]}".replace(u'\xa0', u'')
+                else:
+                    text = f"Что-то пошло не так. Нет ни описания, ни комментариев ¯\_(ツ)_/¯"
+            except IndexError as err:
+                continue
+        write_to_db.append((link, text))
+
+    print(write_to_db)
+
+    print(f'Время выполнения парсинга: {time.time() - start_time} секунд')
