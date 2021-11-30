5a1cd1f51ba235b2cbda4b958d223fd3c002fe62
Maxim Vinokurov <vmstr8@yandex.ru>
2021-12-01 01:34:51 +0300

    Сделали запись в базу данных

--- vkbot_parser/parser_app.py
+++ vkbot_parser/parser_app.py
@@ -2,6 +2,8 @@ import time
 
 from vkbot_parser.vk_parser import AlbumsParser
 
+from settings import AUTH_TOKEN
+
 app = AlbumsParser()
 
 if __name__ == '__main__':
@@ -10,28 +12,29 @@ if __name__ == '__main__':
     photos_and_comments = app.get_comments(group_name='esg', all_photos=photos)
 
     write_to_db = []
-
+    # TODO удаление из базы
+    # TODO селект из базы чтобы показывать боту при запросе пользователем той или иной хуйни
     for value in photos_and_comments.values():
         link = value['link']
         if value['text'] != '':
             try:
                 if value['comments'][-1] != '':
-                    text = f"Описание фото: {value['text']}.\n\n1-ый комментарий к фото: " \
+                    text = f"Описание фото:\n{value['text']}.\n\n1-ый комментарий к фото:\n" \
                            f"{value['comments'][-1]}".replace(u'\xa0', u'')
                 else:
-                    text = f"Описание фото: {value['text']}".replace(u'\xa0', u'')
+                    text = f"Описание фото:\n{value['text']}".replace(u'\xa0', u'')
             except IndexError as err:
-                continue
+                text = f"Описание фото:\n{value['text']}".replace(u'\xa0', u'')
         else:
             try:
                 if value['comments'][-1] != '':
-                    text = f"1-ый комментарий к фото: {value['comments'][-1]}".replace(u'\xa0', u'')
+                    text = f"1-ый комментарий к фото:\n{value['comments'][-1]}".replace(u'\xa0', u'')
                 else:
                     text = f"Что-то пошло не так. Нет ни описания, ни комментариев ¯\_(ツ)_/¯"
             except IndexError as err:
-                continue
-        write_to_db.append((link, text))
-
-    print(write_to_db)
-
+                text = f"Что-то пошло не так. Нет ни описания, ни комментариев ¯\_(ツ)_/¯"
+        write_to_db.append({"link": link, "text": text})
+    for item in write_to_db:
+        requests.post(url='http://127.0.0.1:8080/add_items/', headers={'auth': AUTH_TOKEN}, json=item)
+    print(len(write_to_db))
     print(f'Время выполнения парсинга: {time.time() - start_time} секунд')
