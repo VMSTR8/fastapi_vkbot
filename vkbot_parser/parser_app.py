111319374d3d99362021b0432e72c81b8db74d69
Maxim Vinokurov <vmstr8@yandex.ru>
2021-12-15 00:20:55 +0300

    Оптимизация файла парсера. Добавлено удаление из базы прежде чем собирать и делать новые записи в базу

--- vkbot_parser/parser_app.py
+++ vkbot_parser/parser_app.py
@@ -4,17 +4,19 @@ import requests
 
 from vkbot_parser.vk_parser import AlbumsParser
 
+from vkbot_sql.crud import delete_all_items
+from get_db import get_db
+
 from settings import AUTH_TOKEN
 
 app = AlbumsParser()
 
 if __name__ == '__main__':
     start_time = time.time()
+    delete_all_items(next(get_db()))
     photos = app.get_photos(group_name='esg')
     photos_and_comments = app.get_comments(group_name='esg', all_photos=photos)
-
     write_to_db = []
-    # TODO удаление из базы
     for value in photos_and_comments.values():
         link = value['link']
         if value['text'] != '':
@@ -36,6 +38,6 @@ if __name__ == '__main__':
                 text = f"Что-то пошло не так. Нет ни описания, ни комментариев ¯\_(ツ)_/¯"
         write_to_db.append({"link": link, "text": text})
     for item in write_to_db:
-        requests.post(url='http://127.0.0.1:8080/add_items/', headers={'auth': AUTH_TOKEN}, json=item)
-    print(len(write_to_db))
+        requests.post(url='http://127.0.0.1:9150/add_items/', headers={'auth': AUTH_TOKEN}, json=item)
+    print(f'Вхождений в базу: {len(write_to_db)}')
     print(f'Время выполнения парсинга: {time.time() - start_time} секунд')
