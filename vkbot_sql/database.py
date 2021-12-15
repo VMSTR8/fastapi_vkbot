ae1e5b42f57feff17e88551e1c51349caf3fe5ff
Maxim Vinokurov <vmstr8@yandex.ru>
2021-12-15 19:27:44 +0300

    Добавлены аргументы pool_size и max_overflow

--- vkbot_sql/database.py
+++ vkbot_sql/database.py
@@ -6,7 +6,7 @@ from settings import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20, max_overflow=0)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
