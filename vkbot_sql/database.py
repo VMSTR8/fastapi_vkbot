220766210831aa702c38ef14b4d25bf63603026d
Maxim Vinokurov <vmstr8@yandex.ru>
2021-12-20 02:01:02 +0300

    Мелкие исправления ошибок

--- vkbot_sql/database.py
+++ vkbot_sql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from settings import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+from settings.settings import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
